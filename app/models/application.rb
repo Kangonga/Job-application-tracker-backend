2e9832fd65622e124b1ea8c60e7bbeea114a70ff
mercylynn <mercy.kamene@student.moringaschool.com>
2022-12-06 15:31:24 +0300

    modify validation

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,5 @@ class Application < ApplicationRecord
     belongs_to :user 
     belongs_to :job
 
-    validates :application_stage, inclusion: {in: ["submitted", "phone-screen", "interview", "accepted"]}
+    validates :application_stage, inclusion: {in: ["submitted", "phone_screen", "interview", "accepted"]}
 end
