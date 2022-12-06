76a4347c3a498827dfc74359db0ec99339700e4d
mercylynn <mercy.kamene@student.moringaschool.com>
2022-12-06 15:27:18 +0300

    active record authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::API
+include ActionController::Cookies
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+before_action :authorize
+    
+private
 
-    # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-    # # skip_before_action :verify_authenticity_token
+def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+end
+     
+def authorize 
+    @current_user = User.find_by(id: session[:user_id]) 
 
-    # def render_unprocessable_entity_response(exception)
-    #     render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
-    # end
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
+end
+
+
 end
