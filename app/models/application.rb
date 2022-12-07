e5e9552b0b304049038df4030eaaebf472977821
mercylynn <mercy.kamene@student.moringaschool.com>
2022-12-07 12:17:18 +0300

    added post and update methods

--- app/models/application.rb
+++ app/models/application.rb
@@ -3,4 +3,6 @@ class Application < ApplicationRecord
     belongs_to :job
 
     validates :application_stage, inclusion: {in: ["submitted", "phone_screen", "interview", "accepted"]}
+    # validates :application_stage, inclusion: { in: %w(submitted Weak Average submitted phone_screen interview accepted),
+    # message: "%{value} is not a valid application_stage" }
 end
